f12a7a92a772c3b5277d74fe319ec695320be958
Z-SooS <zoli.soos98@gmail.com>
2022-11-09 14:29:20 +0100

    Created PropertyReader class to read environmental values

---
+++ src/test/java/utility/PropertyReader.java
@@ -0,0 +1,31 @@
+package utility;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class PropertyReader {
+    private static Properties properties = null;
+
+    public static String getProperty(String propertyName) {
+        String value = System.getProperty(propertyName);
+        if (value != null) return value;
+        if (properties == null) readProperties();
+        value = properties.getProperty(propertyName);
+        if (value != null) return value;
+        throw new RuntimeException("Property " + propertyName + " not set");
+    }
+
+    private static void readProperties() {
+        try (InputStream input = Files.newInputStream(Paths.get("config.properties"))) {
+            Properties prop = new Properties();
+            prop.load(input);
+
+            properties = prop;
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
