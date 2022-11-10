7d5b07d5cd4eb41526340e7b6110f383b7a39c28
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2022-11-10 13:28:12 +0100

    versionWithGlass feature added and StepDefinitions package added

---
+++ src/test/java/StepDefinitions/VersionWithGlass.java
@@ -0,0 +1,39 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class VersionWithGlass {
+    @Given("i am logged in")
+    public void iAmLoggedIn() {
+    }
+
+    @Given("i am on glass page")
+    public void iAmOnGlassPage() {
+    }
+
+    @When("open the provided url for {string}")
+    public void openTheProvidedUrlFor(String arg0) {
+    }
+
+    @When("click on visible issue with provided name {string}")
+    public void clickOnVisibleIssueWithProvidedName(String arg0) {
+    }
+
+    @When("click on edit button")
+    public void clickOnEditButton() {
+    }
+
+    @When("select {string} under {string} menu point")
+    public void selectUnderMenuPoint(String arg0, String arg1) {
+    }
+
+    @When("click on {string} button")
+    public void clickOnButton(String arg0) {
+    }
+
+    @Then("issue with summary {string} is visible")
+    public void issueWithSummaryIsVisible(String arg0) {
+    }
+}
