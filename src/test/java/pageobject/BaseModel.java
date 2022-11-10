85ffcde70535b38b3d954c49d95c10d30bc993ff
Z-SooS <zoli.soos98@gmail.com>
2022-11-10 10:20:04 +0100

    Create base page object model

---
+++ src/test/java/pageobject/BaseModel.java
@@ -0,0 +1,46 @@
+package pageobject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import utility.PropertyReader;
+import utility.WebDriverFactory;
+
+import java.net.MalformedURLException;
+
+public class BaseModel {
+    @FindBy(id = "login-form-username")
+    private WebElement loginInputUsername;
+
+    @FindBy(id = "login-form-password")
+    private WebElement loginInputPassword;
+
+    @FindBy(id = "login-form-submit")
+    private WebElement loginSubmitButton;
+
+    protected WebDriver webDriver;
+
+    public BaseModel() throws MalformedURLException {
+        webDriver = WebDriverFactory.getInstance().getWebDriver();
+    }
+
+    public void quitSession() {
+        WebDriverFactory.getInstance().quitWebDriver();
+    }
+
+    public void doLogin() {
+        webDriver.get(PropertyReader.getProperty("base_url") + "/login.jsp");
+        loginInputUsername.sendKeys(PropertyReader.getProperty("username"));
+        loginInputPassword.sendKeys(PropertyReader.getProperty("password"));
+
+        loginSubmitButton.click();
+    }
+
+    public void openPath(String path) {
+        webDriver.get(PropertyReader.getProperty("base_url") + path);
+    }
+
+    public void maximizeWindow() {
+        webDriver.manage().window().maximize();
+    }
+}
