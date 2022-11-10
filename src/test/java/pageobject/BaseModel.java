77cd1dabc312db4642c9e3d39261188447066dd2
Z-SooS <zoli.soos98@gmail.com>
2022-11-10 11:30:01 +0100

    Implement method to change tabs on glass

--- src/test/java/pageobject/BaseModel.java
+++ src/test/java/pageobject/BaseModel.java
@@ -1,5 +1,7 @@
 package pageobject;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NotFoundException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,6 +9,7 @@ import utility.PropertyReader;
 import utility.WebDriverFactory;
 
 import java.net.MalformedURLException;
+import java.util.List;
 
 public class BaseModel {
     @FindBy(id = "login-form-username")
@@ -17,6 +20,9 @@ public class BaseModel {
 
     @FindBy(id = "login-form-submit")
     private WebElement loginSubmitButton;
+
+    @FindBy(xpath = "//header[@role='banner']/nav")
+    private List<WebElement> tabContainers;
 
     protected WebDriver webDriver;
 
@@ -47,5 +53,15 @@ public class BaseModel {
     public void openGlassDocumentation() {
         webDriver.get(PropertyReader.getProperty("base_url") +
                 "/projects/POK?selectedItem=com.metainf.jira.plugin:glass-project-documentation#/home/general/schemes");
+    }
+
+    public void clickOnTab(String tabName) {
+        for (WebElement navbar : tabContainers) {
+            try {
+                navbar.findElement(By.xpath(String.format("//span[text() = '%s'",tabName)));
+                break;
+            } catch (NotFoundException ignored) {}
+        }
+        throw new NotFoundException("No such element exists in tabs: " + tabName);
     }
 }
